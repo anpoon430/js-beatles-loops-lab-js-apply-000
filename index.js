8686043bcbbc93b16170e1f0d8d648eb87558f9a
Andy Poon <anpoon430@gmail.com>
2018-05-19 18:22:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musicians,instruments){
   var output = [];
   for (var i = 0; i < musicians.length; i++){
-    output.push(musicians[i]+ " " + instruments[i])
+    output.push(musicians[i]+ " plays " + instruments[i]);
   }
+  return output;
 }
